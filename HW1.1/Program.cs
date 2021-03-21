b433fd982656c69f9f413014415659d0ecedc768
fata1-err0r <29376868+fata1-err0r@users.noreply.github.com>
2021-03-21 21:06:04 +0700

    isn't complete

--- HW1.1/Program.cs
+++ HW1.1/Program.cs
@@ -6,7 +6,22 @@ namespace HW1._1
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[] nums = new int[] { 3, 6, 9, 12, 15 }; // целочисленный упорядоченный по возрастанию массив
+			int count = 1; // подсчет
+
+			for (int a = 0; a < nums.Length - 1; a++)
+			{
+				for (int b = a + 1; b < nums.Length; b++)
+				{
+					if (nums[a] == nums[b])
+						break;
+
+					if (b == nums.Length - 1)
+						count++;
+				}
+			}
+			Console.WriteLine($"found {count} different numbers"); // вывод результата в консоль
+			Console.ReadKey();
 		}
 	}
 }
--- HW1.2/Program.cs
+++ HW1.2/Program.cs
@@ -6,7 +6,20 @@ namespace HW1._2
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			int[,] mas = new int[5,5] { { 3, 6, 9, 12, 15 }, { 3, 6, 9, 12, 15 }, { 3, 6, 9, 12, 15 }, { 3, 6, 9, 12, 15 }, { 3, 6, 9, 12, 15 } };
+
+			int rows = mas.GetUpperBound(0) + 1;        // количество рядов
+			int columns = mas.GetUpperBound(1) + 1;     // количество столбцов
+
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < columns; j++)
+				{
+					Console.Write($"{mas[i, j]} \t");
+				}
+				Console.WriteLine();
+			}
+			Console.ReadKey();
 		}
 	}
 }
